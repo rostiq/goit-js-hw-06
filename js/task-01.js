c088a9d5b88510700c5bdad7faa34b1381d1db4a
rostiq <rostiknatkha@gmail.com>
2022-10-10 17:06:13 +0300

    improve
    all 1 and 9 tasks

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,8 @@
-
 const categoriesRef = document.querySelectorAll('.item');
-console.log(`Number of categories: ${categoriesRef.length}`);    
+console.log(`Number of categories: ${categoriesRef.length}`);
 
 categoriesRef.forEach(category => {
-    console.log(`Category: ${category.querySelector('h2').textContent}`);
-    console.log(`Elements: ${category.querySelectorAll('li').length}`);
+    console.log(`Category: ${category.firstElementChild.textContent}`);
+    console.log(`Elements: ${category.lastElementChild.children.length}`);
 });
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,4 +1,4 @@
-function getRandomHexColor() {
+const getRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
